caa7c5e8f4397411b1ad3e9b4ed40f8f98ac20e3
f1renze <f1renze@163.com>
2018-10-16 10:21:08 +0800

    :construction_worker: fix bug :bug: test:whale:

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,4 +6,4 @@ __time__ = '18-5-15 上午10:35'
 # 定义同时开启的处理请求的进程数量, workers = multiprocessing.cpu_count() * 2 + 1
 workers = 3
 worker_class = "gevent"   # 采用gevent库, 支持异步处理请求, 提高吞吐量
-bind = "0.0.0.0:8888"    # 监听IP放宽, 以便于Docker之间/Docker和宿主机之间的通信
+bind = "0.0.0.0:8000"    # 监听IP放宽, 以便于Docker之间/Docker和宿主机之间的通信
